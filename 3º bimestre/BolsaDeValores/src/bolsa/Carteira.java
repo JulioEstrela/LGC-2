4223d3ff927a757329449500364f0fc6409f0003
Julio <julioantunesestrela13@gmail.com>
2023-09-18 00:25:58 -0300

    Revert "Changes~/IFSP/2023/LGC-2/3º bimestre"
    This reverts commit a289e8074cad4b63d4bf36077560b9143521fe4e.

--- 3º bimestre/BolsaDeValores/src/bolsa/Carteira.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Carteira {
-    private List<Ativo> ativos;
-
-    public Carteira(List<Ativo> ativos) {
-        this.ativos = ativos;
-    }
-}
--- 3º bimestre/BolsaDeValores/src/bolsa/FII.java
+++
@@ -1,6 +0,0 @@
-public class FII extends Ativo{
-
-    public FII(String nome, String codigo, double cotacao, int lotePadrao, double dividendos) {
-        super(nome, codigo, cotacao, lotePadrao, dividendos);
-    }
-}
